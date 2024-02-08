4e0b0c5ad4ac62b46722bc0bf485dcb3598b8dff
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-02-08 12:33:00 +0100

    Switch to Nativewindd

--- App.tsx
+++ App.tsx
@@ -1,38 +1,44 @@
-// import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import GroceryItem from './GroceryItem';
+import { Button, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import GroceryItem from './src/components/GroceryItem';
+
+import { NativeWindStyleSheet } from 'nativewind';
+
+NativeWindStyleSheet.setOutput({
+  default: 'native',
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.navbar}>
-        <Button color="#000" title="Add"></Button>
-        <Button color="#000" title="Remove"></Button>
+    <View className="flex-1 flex-column bg-white">
+      <View className="flex-row w-screen justify-center bg-red-950">
         <Button color="#000" title="Clear"></Button>
       </View>
-      <View style={styles.body}>
+      <View className="flex-1 w-screen bg-sky-500">
         <GroceryItem />
         <GroceryItem />
         <GroceryItem />
       </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    flexDirection: 'column',
-  },
-  navbar: {
-    flex: 1,
-    backgroundColor: '#8a8a8a',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  body: {
-    flex: 10,
-    backgroundColor: '#ff0000',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     flexDirection: 'column',
+//   },
+//   navbar: {
+//     flex: 1,
+//     backgroundColor: '#8a8a8a',
+//     flexDirection: 'row',
+//     justifyContent: 'space-evenly',
+//   },
+//   body: {
+//     flex: 10,
+//     backgroundColor: '#ff0000',
+//   },
+// });
